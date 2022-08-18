40142eebcc434aaccaab94bf688269f32054ffde
dim3lagin <100461099+dim3lagin@users.noreply.github.com>
2022-08-18 03:02:01 +0300

    Add files via upload

---
+++ folio/music/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.single-item').slick({
+       
+        prevArrow: '<button class="slider-btn left" id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i> < </button>',
+        nextArrow: '<button class="slider-btn right color1" id="next" type="button" class="btn btn-juliet"> > <i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+    });
+
+});
+
