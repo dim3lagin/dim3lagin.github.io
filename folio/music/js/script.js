743902d5f1ef4ff2400df13555c4541ade9f5755
dim3lagin <100461099+dim3lagin@users.noreply.github.com>
2022-08-18 17:45:08 +0300

    Add files via upload

--- folio/music/js/script.js
+++ folio/music/js/script.js
@@ -1,9 +1,27 @@
-$(function(){
-    $('.single-item').slick({
-       
-        prevArrow: '<button class="slider-btn left" id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i> < </button>',
-        nextArrow: '<button class="slider-btn right color1" id="next" type="button" class="btn btn-juliet"> > <i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
-    });
-
+$(function () {
+  $('.single-item').slick({
+    prevArrow: '<button class="slider-btn left" id="prev" type="button" class="btn btn-juliet"> < </button>',
+    nextArrow: '<button class="slider-btn right color1" id="next" type="button" class="btn btn-juliet"> > </button>'
+  });
+  $('.slider').slick({
+		arrows:true,
+    Infinity: false,
+		dots:false,
+		slidesToShow:4,
+		autoplay:false,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
-
