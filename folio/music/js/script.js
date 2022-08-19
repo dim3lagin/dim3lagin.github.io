d09467ed781387a908ea29c4ec8ce10306e50c95
dim3lagin <100461099+dim3lagin@users.noreply.github.com>
2022-08-19 07:31:52 +0300

    Add files via upload

--- folio/music/js/script.js
+++ folio/music/js/script.js
@@ -1,31 +1,50 @@
 $(function () {
-  $('.single-item').slick({
-    prevArrow: '<button class="slider-btn left" id="prev" type="button" class="btn btn-juliet"> < </button>',
-    nextArrow: '<button class="slider-btn right color1" id="next" type="button" class="btn btn-juliet"> > </button>'
-  });
-  $('.slider').slick({
-		arrows:true,
-    	infinity: false,
-		dots:false,
-		slidesToShow:4,
-		autoplay:false,
-		responsive:[
-			{
+	$('.single-item').slick({
+		prevArrow: '<button class="slider-btn left" id="prev" type="button" class="btn btn-juliet"> < </button>',
+		nextArrow: '<button class="slider-btn right color1" id="next" type="button" class="btn btn-juliet"> > </button>'
+	});
+
+	/*Burger on mobile*/
+
+	let navToggle = $('#navToggle');
+	let nav = $('#nav');
+
+	navToggle.on('click', function (event) {
+		event.preventDefault();
+
+		$("body").toggleClass('show-nav');
+		$(this).toggleClass('active');
+		nav.toggleClass('show');
+	});
+
+	$(window).on("resize", function () {
+		$("body").removeClass('show-nav');
+		navToggle.removeClass('active');
+		nav.removeClass('show');
+	});
+
+	$('.slider').slick({
+		arrows: true,
+		infinity: false,
+		dots: false,
+		slidesToShow: 4,
+		autoplay: false,
+		responsive: [{
 				breakpoint: 1067,
 				settings: {
-					slidesToShow:3
+					slidesToShow: 3
 				}
 			},
 			{
 				breakpoint: 803,
 				settings: {
-					slidesToShow:2
+					slidesToShow: 2
 				}
 			},
 			{
 				breakpoint: 550,
 				settings: {
-					slidesToShow:1
+					slidesToShow: 1
 				}
 			}
 		]
