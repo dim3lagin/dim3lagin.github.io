4c00e533c81c39aad3a4243448371e010fd925bc
dim3lagin <100461099+dim3lagin@users.noreply.github.com>
2022-08-27 19:41:13 +0300

    Add files via upload

--- folio/music/js/script.js
+++ folio/music/js/script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function first () {
 	$('.single-item').slick({
 		prevArrow: '<button class="slider-btn left" id="prev" type="button" class="btn btn-juliet"> < </button>',
 		nextArrow: '<button class="slider-btn right color1" id="next" type="button" class="btn btn-juliet"> > </button>'
@@ -98,6 +98,21 @@ $(function () {
             modal.removeClass('show-modal');
         }, 200);
     }
-
-
+	
 });
+
+function readmore() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.getElementById("myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Read more";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less";
+	  moreText.style.display = "inline";
+	}
+  }
