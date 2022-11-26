e4826f02433afbf4b78d2985a844b2461a1e340f
dim3lagin <100461099+dim3lagin@users.noreply.github.com>
2022-11-26 21:00:10 +0300

    Add files via upload

--- folio/KarelDirve/js/script.js
+++ folio/KarelDirve/js/script.js
@@ -1,5 +1,4 @@
 /*Burger action*/
-
 let navToggle = $('#navToggle');
 let nav = $('#nav');
 
@@ -7,18 +6,17 @@ navToggle.on('click', function (event) {
     event.preventDefault();
 
     $("body").toggleClass('show-nav');
-    $(this).toggleClass('activee');
-    nav.toggleClass('nav-active');
+    $(this).toggleClass('active');
+    nav.toggleClass('nav-burger-inner-active');
 });
 
 $(window).on("resize", function () {
     $("body").removeClass('show-nav');
-    navToggle.removeClass('activee');
-    nav.removeClass('nav-active');
+    navToggle.removeClass('active');
+    nav.removeClass('nav-burger-inner-active');
 });
 
 /*Cars Changes*/
-
 const tabsBtn = document.querySelectorAll(".tab-btn");
 const tabsItem = document.querySelectorAll(".tabs");
 
@@ -29,62 +27,32 @@ tabsBtn.forEach(function (item) {
         let currentTab = document.querySelector(tabId);
 
         tabsBtn.forEach(function (item) {
-            item.classList.remove('active');
+            item.classList.remove('active-item');
         });
 
         tabsItem.forEach(function (item) {
             item.classList.remove('flex-active');
         });
 
-        currentBtn.classList.add('active');
+        currentBtn.classList.add('active-item');
         currentTab.classList.add('flex-active');
     });
 });
 
-let header = $("#header");
-let headerH = header.innerHeight();
-let scrollTop = $(window).scrollTop();
+/*Close/open answers*/
+const answBtn = document.querySelectorAll(".title-js");
+const answItem = document.querySelectorAll(".answer-js");
 
-/* Smooth Scroll*/
+answBtn.forEach(function (item) {
+    item.addEventListener("click", function () {
+        let currentAnswBtn = document.querySelector(".answer");
+        let currentAnswId = currentAnswBtn.getAttribute("data-open");
+        let currentAnswTab = document.querySelector(currentAnswId);
 
-$("[data-scroll]").on("click", function (event) {
-    event.preventDefault();
+        answBtn.forEach(function (currentAnswBtn) {
+            currentAnswBtn.classList.remove('answer-active');
+        });
 
-    let scrollEl = $(this).data("scroll");
-    let scrollElPos = $(scrollEl).offset().top;
-
-    $("body").removeClass('show-nav');
-    navToggle.removeClass('active');
-    nav.removeClass('show');
-
-    $("html, body").animate({
-        scrollTop: scrollElPos - headerH
-    }, 500)
-});
-
-/* ScrollSpy*/
-let windowH = $(window).height();
-scrollSpy(scrollTop);
-
-$(window).on("scroll", function () {
-    scrollTop = $(this).scrollTop();
-    scrollSpy(scrollTop);
-});
-
-function scrollSpy(scrollTop) {
-    $("[data-scrollspy]").each(function () {
-        let $this = $(this);
-        let sectionId = $this.data('scrollspy');
-        let sectionOffset = $this.offset().top;
-        sectionOffset = sectionOffset - (windowH * 0.55555);
-
-        if (scrollTop >= sectionOffset) {
-            $('#nav [data-scroll]').removeClass('active-nav-link');
-            $('#nav [data-scroll="' + sectionId + '"]').addClass('active-nav-link');
-        }
-
-        if (scrollTop == 0) {
-            $('#nav [data-scroll]').removeClass('active-nav-link');
-        }
+        currentAnswBtn.classList.add('answer-active');
     });
-}
+});
