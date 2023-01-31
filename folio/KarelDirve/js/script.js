a012d45c993eb05e4aa5a2ed2e721d9cb357a031
dim3lagin <100461099+dim3lagin@users.noreply.github.com>
2023-01-31 18:07:02 +0300

    Add files via upload

--- folio/KarelDirve/js/script.js
+++ folio/KarelDirve/js/script.js
@@ -7,14 +7,17 @@ navToggle.on('click', function (event) {
 
     $("body").toggleClass('show-nav');
     $(this).toggleClass('activee');
-    nav.toggleClass('nav-burger-inner-active');
+    nav.toggleClass('nav-show');
 });
 
 $(window).on("resize", function () {
     $("body").removeClass('show-nav');
     navToggle.removeClass('activee');
-    nav.removeClass('nav-burger-inner-active');
+    nav.removeClass('nav-show');
 });
+
+
+
 
 /*Cars Changes*/
 const tabsBtn = document.querySelectorAll(".tab-btn");
@@ -56,3 +59,60 @@ answBtn.forEach(function (item) {
         currentAnswBtn.classList.add('answer-active');
     });
 });
+
+
+    let header = $("#header");
+    let headerH = header.innerHeight();
+    let scrollTop = $(window).scrollTop();
+
+
+
+    /* Smooth Scroll to sections
+    =====================================*/
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let scrollEl = $(this).data("scroll");
+        let scrollElPos = $(scrollEl).offset().top;
+
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('nav-show');
+
+        $("html, body").animate({
+            scrollTop: scrollElPos - headerH
+        }, 500)
+    });
+
+
+
+
+    /* ScrollSpy
+    =====================================*/
+    let windowH = $(window).height();
+    scrollSpy(scrollTop);
+
+    $(window).on("scroll", function() {
+        scrollTop = $(this).scrollTop();
+        scrollSpy(scrollTop);
+    });
+
+    function scrollSpy(scrollTop) {
+        $("[data-scrollspy]").each(function() {
+            let $this = $(this);
+            let sectionId = $this.data('scrollspy');
+            let sectionOffset = $this.offset().top;
+            sectionOffset = sectionOffset - (windowH * 0.5);
+
+            if(scrollTop >= sectionOffset) {
+                $('#nav [data-scroll]').removeClass('active');
+                $('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+            }
+
+            if(scrollTop == 0) {
+                $('#nav [data-scroll]').removeClass('active');
+            }
+        });
+    }
+
